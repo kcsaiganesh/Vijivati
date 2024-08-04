0e34caff171b18fd607601816f16d16505f5d27c
Sai Ganesh <kcsaiganesh@gmail.com>
2024-08-04 11:26:14 +0530

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+      },
+      colors: {
+        text: "#0a0a0a",
+        background: "#b7e4c7",
+        primary: "#2d6a4f",
+        secondary: "#52b788",
+        cta: "#dddf00",
+        light: "#d8f3dc",
+        dark: "#081c15",
       },
     },
   },
